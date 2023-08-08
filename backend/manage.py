43ea5c619b9dfa2f39684f5d953f054a5d35a64c
Hana <hana.benami@gmail.com>
2023-08-08 20:31:35 +0300

    Move secrets to EnvKey

--- backend/manage.py
+++ backend/manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
+import envkey
 import os
 import sys
 
--- docker/backend/celery-entrypoint.sh
+++ docker/backend/celery-entrypoint.sh
@@ -2,6 +2,7 @@
 
 WORKERS=2
 echo "Start $WORKERS celery workers"
+sed -i -e 's/import sys/import sys, envkey/g' /usr/local/bin/celery
 useradd -ms /bin/bash celery
 chown -R celery:celery /app
 su celery -c "celery -A base worker --loglevel=info --concurrency $WORKERS -E"
